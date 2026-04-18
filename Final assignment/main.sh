d904e7020b68eae1190e0c7a68b06c0f12ecb8de
liyaboro <l.boro@student.tue.nl>
2026-04-18 12:55:55 +0200

    update slurm

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,9 +3,9 @@ wandb login
 python3 -u train.py \
     --data-dir ./data/cityscapes \
     --batch-size 32 \
-    --epochs 5 \
+    --epochs 10 \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplabv3plus-resnet101-ft-baseline-test" \
+    --experiment-id "deeplab-resnet101-ft-baseline-randomcrop" \
     --pretrained-ckpt ./checkpoints/deeplab_resnet101_os16_base.pth.tar
