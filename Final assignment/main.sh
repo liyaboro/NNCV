7ff640e6745b6dd9240b6e49b2918a1bf69e28b0
liyaboro <l.boro@student.tue.nl>
2026-04-19 17:56:06 +0200

    new slurm files

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,9 +3,9 @@ wandb login
 python3 -u train.py \
     --data-dir ./data/cityscapes \
     --batch-size 32 \
-    --epochs 30 \
+    --epochs 50 \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplab-resnet101-ft-baseline-rc+dice+scheduler" \
+    --experiment-id "deeplab-resnet101-ft-baseline-rc+dice+scheduler2" \
     --pretrained-ckpt ./checkpoints/deeplab_resnet101_os16_base.pth.tar
