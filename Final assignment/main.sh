d10ccb1d532cf4de43e2bcb06f715ab2d596ef19
liyaboro <l.boro@student.tue.nl>
2026-04-25 16:34:51 +0200

    change thresh

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -8,6 +8,6 @@ python3 -u train_ood.py \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplab-resnet101-ft-ood-v2-70" \
-    --ood-threshold-percentile 70.0 \
+    --experiment-id "deeplab-resnet101-ft-ood-v2-92" \
+    --ood-threshold-percentile 92.0 \
     --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-ood-v2-90/best_ood_model-epoch=0027-val_loss=0.006985788146266714.pt
