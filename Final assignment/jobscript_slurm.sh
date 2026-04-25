dd9b75d1e121e661597448f98fcb64e3a7804ddf
liyaboro <l.boro@student.tue.nl>
2026-04-25 15:47:03 +0200

    change thresh

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -4,6 +4,6 @@
 #SBATCH --cpus-per-task=18  
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=01:15:00
+#SBATCH --time=00:15:00
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,10 +4,10 @@ python3 -u train_ood.py \
     --data-dir ./data/cityscapes \
     --mode ood \
     --batch-size 32 \
-    --epochs 30 \
+    --epochs 0 \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplab-resnet101-ft-ood-v2-90" \
-    --ood-threshold-percentile 90.0 \
-    --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-segv4/best_model-epoch=0036-val_loss=0.22219626978039742.pt
+    --experiment-id "deeplab-resnet101-ft-ood-v2-95" \
+    --ood-threshold-percentile 95.0 \
+    --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-ood-v2-90/best_ood_model-epoch=0027-val_loss=0.006985788146266714.pt
