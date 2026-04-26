0cbbf349eae99b528500acde3fb876f7ce094074
liyaboro <l.boro@student.tue.nl>
2026-04-26 16:25:17 +0200

    update thresh in slurm

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -8,6 +8,6 @@ python3 -u train_ood.py \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplab-resnet101-ft-ood-v2-90" \
-    --ood-threshold-percentile 90.0 \
+    --experiment-id "deeplab-resnet101-ft-ood-v3-80" \
+    --ood-threshold-percentile 80.0 \
     --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-ood-v3-95/final_best_model-epoch=0036-val_loss=0.0077.pt
