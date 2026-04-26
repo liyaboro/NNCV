0b5a22f196a41735551fca983834abd6360a4286
liyaboro <l.boro@student.tue.nl>
2026-04-26 15:49:23 +0200

    reset thresh

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,9 +4,10 @@ python3 -u train_ood.py \
     --data-dir ./data/cityscapes \
     --mode ood \
     --batch-size 32 \
-    --epochs 40 \
+    --epochs 0 \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "deeplab-resnet101-ft-ood-v3-90" \
-    --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-segv4/best_model-epoch=0036-val_loss=0.22219626978039742.pt
+    --experiment-id "deeplab-resnet101-ft-ood-v2-90" \
+    --ood-threshold-percentile 90.0 \
+    --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-ood-v3-95/final_best_model-epoch=0036-val_loss=0.0077.pt
---
+++ Final assignment/main_ood _only.sh
@@ -0,0 +1,12 @@
+wandb login
+
+python3 -u train_ood.py \
+    --data-dir ./data/cityscapes \
+    --mode ood \
+    --batch-size 32 \
+    --epochs 40 \
+    --lr 1e-4 \
+    --num-workers 10 \
+    --seed 42 \
+    --experiment-id "deeplab-resnet101-ft-ood-v3-90" \
+    --pretrained-ckpt ./checkpoints/deeplab-resnet101-ft-segv4/best_model-epoch=0036-val_loss=0.22219626978039742.pt
